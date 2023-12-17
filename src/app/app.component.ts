29374f02599e0140428ab75599bcc13500a811c6
Amari-sara2001 <96389043+Amari-sara2001@users.noreply.github.com>
2023-12-17 23:09:36 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ListEmpComponent} from "./compnents/list-emp/list-emp.component";
+import {DetailEmpComponent} from "./compnents/detail-emp/detail-emp.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ListEmpComponent, DetailEmpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {ListEmpComponent} from "./compnents/list-emp/list-emp.component";
+import {NewEmpComponent} from "./compnents/new-emp/new-emp.component";
+import {DetailEmpComponent} from "./compnents/detail-emp/detail-emp.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'list', component : ListEmpComponent},
+  {path: 'add', component : NewEmpComponent},
+
+  {path:'update/:id',component:DetailEmpComponent},
+  {path: '', redirectTo : '/list' , pathMatch:'full'},
+];
